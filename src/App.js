2eea87467c1f241453c530859218698a6160394c
greenbookwebb <51996455+greenbookwebb@users.noreply.github.com>
2023-02-22 11:47:24 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ReactTerminal } from "react-terminal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [theme, setTheme] = React.useState("dark");
+  const [controlBar, setControlBar] = React.useState(true);
+  const [controlButtons, setControlButtons] = React.useState(true);
+  const [prompt, setPrompt] = React.useState(">>>");
+  
+  const commands = {
+    help: (
+      <span>
+        <strong>clear</strong> - clears the console. <br />
+        <strong>change_prompt &lt;PROMPT&gt;</strong> - Change the prompt of the
+        terminal. <br />
+        <strong>change_theme &lt;THEME&gt;</strong> - Changes the theme of the
+        terminal. Allowed themes - light, dark, material-light, material-dark,
+        material-ocean, matrix and dracula. <br />
+        <strong>toggle_control_bar</strong> - Hides / Display the top control
+        bar. <br />
+        <strong>toggle_control_buttons</strong> - Hides / Display the top
+        buttons on control bar. <br />
+        <strong>evaluate_math_expression &lt;EXPR&gt;</strong> - Evaluates a
+        mathematical expression (eg, <strong>4*4</strong>) by hitting a public
+        API, api.mathjs.org.
+      </span>
+    ),
+
+    change_prompt: (prompt) => {
+      setPrompt(prompt);
+    },
+
+    change_theme: (theme) => {
+      const validThemes = [
+        "light",
+        "dark",
+        "material-light",
+        "material-dark",
+        "material-ocean",
+        "matrix",
+        "dracula",
+      ];
+      if (!validThemes.includes(theme)) {
+        return `Theme ${theme} not valid. Try one of ${validThemes.join(", ")}`;
+      }
+      setTheme(theme);
+    },
+
+    toggle_control_bar: () => {
+      setControlBar(!controlBar);
+    },
+
+    toggle_control_buttons: () => {
+      setControlButtons(!controlButtons);
+    },
+
+    evaluate_math_expression: async (expr) => {
+      const response = await fetch(
+        `https://api.mathjs.org/v4/?expr=${encodeURIComponent(expr)}`
+      );
+      return await response.text();
+    },
+  };
+
+  const welcomeMessage = (
+    <>
+    <h1 style={{ fontWeight: "bold" }}>
+      Welcome to Lachlan Webb's personal website. <br /></h1>
+      <h3>
+      Type "help" for all available commands. <br />
+    </h3>
+    </>
   );
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (<div className="App-header"><ReactTerminal
+  prompt={prompt}
+  theme={theme}
+  showControlBar={controlBar}
+  showControlButtons={controlButtons}
+  welcomeMessage={welcomeMessage}
+  commands={commands}
+  defaultHandler={(command, commandArguments) => {
+    return `${command} passed on to default handler with arguments ${commandArguments}`;
+  }}
+/>
+  </div>);
 }
+
+
 
 export default App;
